0d3798286bc96e4faee9e3cfb3b3dc632fa22a52
Ramprasad <vrushabh.patle@thrymr.net>
2022-09-23 17:49:21 +0530

    swagger issue fixed

--- authentication/src/main/java/net/thrymr/security/WebSecurityConfig.java
+++ authentication/src/main/java/net/thrymr/security/WebSecurityConfig.java
@@ -51,7 +51,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/admin/**").permitAll()
