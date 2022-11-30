4e60ee4b62e8b623f231c9559a3f88c4ae048aa9
onegit config --global user.email 1@gmail.orgggit config --global user.password 1234321save <veeresh.chanda@thrymr.net>
2022-11-30 12:28:05 +0530

    changes in moodinfo graphql and paginationResponse

---
+++ common/src/main/java/net/thrymr/dto/PaginationResponse.java
@@ -0,0 +1,31 @@
+package net.thrymr.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.thrymr.model.*;
+import net.thrymr.model.master.MtMeditation;
+import net.thrymr.model.master.MtWorksheet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class PaginationResponse {
+     private List<Site> siteList=new ArrayList<>();
+        private List<Team> teamList=new ArrayList<>();
+        private Set<Vendor> vendorList=new HashSet<>();
+        private List<AppUser> appUserList=new ArrayList<>();
+        private List<MiniSession> miniSessionList=new ArrayList<>();
+        private List<MtMeditation> meditationList=new ArrayList<>();
+        private List<MtWorksheet> worksheetList=new ArrayList<>();
+        private Set<Chapter> chapterList= new HashSet<>();
+        private List<Unit> unitList = new ArrayList<>();
+        private int totalPages=0;
+        private Long totalElements=0L;
+
+}
