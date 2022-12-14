e83cbc637d4bf25c78c7cdf4dc32350d30f8956a
Venkat <venkatan@dynamind.co>
2022-12-14 11:02:48 +0530

    Merge conflicts resolved

---
+++ common/src/main/java/net/thrymr/dto/CounsellorSlotDto.java
@@ -0,0 +1,32 @@
+package net.thrymr.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.thrymr.enums.SlotShift;
+import net.thrymr.enums.SlotStatus;
+import net.thrymr.model.AppUser;
+import net.thrymr.model.Counsellor;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor
+public class CounsellorSlotDto {
+    private Long counsellorId;
+    private String startTime;
+    private String endTime;
+    private String slotDate;
+    private List<SlotShift> slotShift;
+    private List<DayOfWeek> slotDays;
+    private String slotStatus;
+    private Long appUserId;
+    private String fromDate;
+    private String toDate;
+    private Boolean isActive;
+    private Long counsellorSlotId;
+}
